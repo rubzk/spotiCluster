e9a43fc299fc2590a0c7480d0b57360e42012244
tertola <tertola@leafnoise.io>
2020-09-14 12:52:16 -0300

    First Plot in front-end

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, url_for, request
 import requests 
 from utils.extract import authenticate, extract_all_playlist, extract_all_tracks, get_audio_features
 import credentials
-from utils.transform import concat_data
+from utils.transform import concat_data, scale_data, clustering, plot3d
 app = Flask(__name__)
 
 
@@ -20,10 +20,12 @@ def auth_ok():
     tracks = extract_all_tracks(auth_token, playlists)
     audio_ft = get_audio_features(auth_token, tracks)
     data = concat_data(tracks,audio_ft)
+    scaled = scale_data(data, ['valence','energy','danceability'])
+    scaled_cluster = clustering(scaled, 4)
+    scatter = plot3d(scaled_cluster)
 
-    return '{}'.format(data)
+    return render_template('plot.html', plot=scatter)
 
-    
 if __name__ == '__main__':
     app.run(debug=True)
 
--- utils/transform.py
+++ utils/transform.py
@@ -1,6 +1,11 @@
 import pandas as pd 
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
+import plotly.graph_objects as go 
+import json
+from plotly.utils import PlotlyJSONEncoder
 
 def concat_data(df_info_tracks, df_audio_ft):
 
@@ -8,6 +13,8 @@ def concat_data(df_info_tracks, df_audio_ft):
     df_audio_ft.reset_index(drop=True, inplace=True)
 
     df_join = pd.concat([df_info_tracks, df_audio_ft], axis =1)
+
+    df_join.to_csv('output.csv')
 
     return df_join
 
@@ -21,3 +28,45 @@ def scale_data(df, features):
     scaled_df.columns = features
 
     return scaled_df
+
+
+def get_cluster_number(df_scaled):
+    
+    df = pd.DataFrame([], columns=['SSD', 'n_clusters'])
+
+    for n  in range(1,15):
+        kmeans = KMeans(n_clusters=n)
+        kmeans.fit(X=df_scaled)
+        df = df.append({'SSD': kmeans.inertia_, 'n_clusters': n}, ignore_index=True)
+
+    return 4
+
+
+def clustering(df_scaled, clusters):
+
+    kmeans = KMeans(n_clusters=clusters).fit(df_scaled)
+
+    y_kmeans = kmeans.predict(df_scaled)
+
+
+    df_scaled['cluster'] = y_kmeans
+
+    return df_scaled
+
+
+def plot3d(df_scaled):
+
+    data = [ go.Scatter3d(x=df_scaled['valence'], y=df_scaled['energy'], z=df_scaled['danceability'],
+    marker=dict(color=df_scaled['cluster'])
+    )]
+
+    graphJSON = json.dumps(data, cls=PlotlyJSONEncoder)
+
+    return graphJSON
+
+
+
+
+
+
+
