1de8571a080c930fe9bbb87753b52b7b5c688d4c
tertola <tertola@leafnoise.io>
2020-09-22 22:12:10 -0300

    Added Dockerfile, docker-compose, requirements.txt. Docker image working fine

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def auth_ok():
     return render_template('plot.html', form=json.dumps(form_data, cls=PlotlyJSONEncoder))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
 
