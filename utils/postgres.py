02f85583329f839031f1139f572aa15ae844cc48
rubzk <tomas.ertola@n26.com>
2023-05-09 00:16:53 +0200

    Added class to upload data to postgres db

---
+++ utils/postgres.py
@@ -0,0 +1,69 @@
+import pandas as pd
+from sqlalchemy import text
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+import os
+
+
+class PostgresDB(object):
+
+    def __init__(
+            self,
+            port: int = 5432,
+            db_name: str = 'spoticluster'
+    ):
+        self.db_name = db_name
+        self.port = int(port)
+
+    @property
+    def conn(
+        self
+    ):
+        username = os.environ['POSTGRES_USER']
+        password = os.environ['POSTGRES_PASSWORD']
+        host = os.environ['POSTGRES_HOST']
+
+        if not hasattr(self, "_conn"):
+            url_object = URL.create(
+                'postgresql',
+                username=username,
+                password=password,
+                host=host,
+                port=self.port,
+                database=self.db_name
+            )
+
+            self._conn = create_engine(
+                url_object,
+                echo=True,
+                pool_size=5,
+                max_overflow=0,
+                pool_pre_ping=False,
+            )
+        return self._conn
+    
+    def engine(self):
+        return self.conn 
+    
+
+def df_to_db(
+    database: str = 'postgresql',
+    schema=None,
+    insert_method="append",
+    chunksize=None,
+    method="multi",
+    df,
+    table_name,
+):
+    """Wrap df_to_db function to create engine and write to database table."""
+    db = PostgresDB(database)
+
+    df.to_sql(
+        table_name,
+        con=db.engine(),
+        index=False,
+        if_exists=insert_method,
+        schema=schema,
+        chunksize=chunksize,
+        method=method,
+    )
