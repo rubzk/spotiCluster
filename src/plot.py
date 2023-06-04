a47b9fcdd904e0523391d7486583426a17c8ee45
rubzk <tomas.ertola@n26.com>
2023-06-04 21:09:11 +0200

    removed old radar chart from plots

--- src/plot.py
+++ src/plot.py
@@ -29,19 +29,7 @@ class Plot:
 
         return fig
 
-    # def radar_chart(self, cluster_stats):
-    #     plot = {}
 
-    #     cluster_stats = cluster_stats[["cluster_name"] + self.audio_ft]
-
-    #     for cluster in range(cluster_stats.shape[0]):
-    #         plot[cluster_stats.iloc[cluster]["cluster_name"]] = cluster_stats.iloc[
-    #             cluster, cluster_stats.columns != "cluster_name"
-    #         ].to_list()
-
-    #     plot["categories"] = self.audio_ft
-
-    #     return plot
     
     def radar_chart(self,cluster_stats):
 
