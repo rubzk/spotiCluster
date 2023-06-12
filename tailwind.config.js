562457fa09e9b7c71d86abcf437c095e75c82aa8
rubzk <tomas.ertola@n26.com>
2023-06-12 17:48:22 +0200

    Added fade out animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,21 @@ module.exports = {
         ...fontFamily,
         'sans': ['Helvetica', 'Arial', 'sans-serif'],
       },
+      animation: {
+        fadedown: "fadedown 0.5s"
+      },
+      keyframes: {
+        fadedown: {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(-30px) scale(0.9)",
+          },
+          "100%": {
+            opacity: 100,
+            transform: "translateY(0px) scale(1)",
+          }
+        }
+      }
     },
 
   },
