185a321537a3160a111e4f0b9dca7f3b1178dcc2
rubzk <tomas.ertola@gmail.com>
2023-11-07 00:16:34 +0100

    Deprecated the transform class not longer needed

--- src/transform.py
+++ src/transform.py
@@ -7,60 +7,6 @@ class TransformDataFrame:
         self.df_tracks = df_tracks
         self.df_audio_ft = df_audio_ft
 
-    def concat_data(self):
-        self.df_tracks.reset_index(drop=True, inplace=True)
-        self.df_audio_ft.reset_index(drop=True, inplace=True)
-
-
-        df_join = self.df_tracks.merge(self.df_audio_ft, on="id", how="inner")
-
-        df_join["title"] = df_join["name"] + " - " + df_join["artist"]
-
-        df_join["key"], df_join["is_major"] = self._key_normalization(df_join)
-
-        return df_join
-
-    def _remove_unnecessary_columns(self):
-        pass
-
-    def rename_and_reindex_columns(self, concat_data):
-        renamed_columns = {
-            "id": "song_id",
-            "name": "song_name",
-            "key": "song_key",
-            "mode": "song_mode",
-            "uri": "song_uri",
-        }
-
-        reindex_columns = [
-            "spotify_user_id",
-            "song_id",
-            "song_name",
-            "artist",
-            "title",
-            "song_uri",
-            "song_key",
-            "is_major",
-            "danceability",
-            "energy",
-            "loudness",
-            "speechiness",
-            "acousticness",
-            "instrumentalness",
-            "liveness",
-            "valence",
-            "tempo",
-            "time_signature",
-            "duration_ms",
-            "track_href",
-            "analysis_url",
-            "created",
-        ]
-
-        concat_data = concat_data.rename(columns=renamed_columns)
-
-        return concat_data.reindex(columns=reindex_columns)
-
     def _key_normalization(self, final_df):
         final_df["key"] = final_df["key"].map(
             {
