3d3f1a645ad0d0b32844dfe70acfd562b86e6a65
rubzk <tomas.ertola@n26.com>
2023-11-17 11:21:43 +0100

    Detacched status and results from the same route

--- blueprints/celery_blueprints/celery_blueprint.py
+++ blueprints/celery_blueprints/celery_blueprint.py
@@ -73,37 +73,27 @@ def auth():
         append_results.s(user=user_data.dict()) | cluster_results.s() | create_plots.s()
     )
 
-    return redirect(url_for("celery_bp.taskstatus", celery_task_id=task.id))
+    return redirect(url_for("celery_bp.results_route", celery_task_id=task.id))
 
+@celery_bp.route("/results/<celery_task_id>", methods=["GET"])
+def results_route(celery_task_id):
+    return render_template("plot.html", task_id=celery_task_id)
 
 @celery_bp.route("/status/<celery_task_id>", methods=["GET"])
 def taskstatus(celery_task_id):
     task = current_app.celery.AsyncResult(celery_task_id)
 
-    # app.logger.info(f"status: {task.state}")
+    ## In the future  I would love to have an status bar 
 
     if "application/json" in request.headers.get("Content-Type", ""):
         if task.state == "SUCCESS":
-            # with open("./output/task_info.json", "w") as json_file:
-            #     json.dump(task.info, json_file)
-
             return {"plots": task.info}
 
-        # app.logger.info(task.info)
 
         return {
             "status": task.state
-        }  ### Here One idea I have is to return the status of the task and Update front end with it
-
-    return render_template("plot.html", task_id=celery_task_id)
+        }  
 
 
-# @app.route("/tasks/", methods=["GET"])
-# def get_tasks_celery():
-#     current_app.loader.import_default_modules()
 
-#     tasks = list(
-#         sorted(name for name in current_app.tasks if not name.startswith("celery."))
-#     )
 
-#     return jsonify(tasks)
